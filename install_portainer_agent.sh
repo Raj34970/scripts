9756f76d7ad73cb0054e1543a40ca4fb38575aea
debian <debian@nextcloud-home.lxhome.local>
2025-04-30 15:24:38 +0200

    scripts updated

--- install_portainer_agent.sh
+++ install_portainer_agent.sh
@@ -1,21 +1,9 @@
-services:
-  vaultwarden:
-    image: vaultwarden/server:latest
-    container_name: vaultwarden
-    restart: unless-stopped
-    ports:
-     - 9445:80
-    volumes:
-     - ./bitwarden:/data:rw
-    environment:
-     - ADMIN_TOKEN=v/v7jAgzndj262AyyT+kQcgEUIb9VcZUzIoHESayUqP/umrbbsPAuQGYgWn2JmiK
-     - WEBSOCKET_ENABLED=true
-     - SIGNUPS_ALLOWED=true
-     - SMTP_HOST=smtp.lxhome.fr
-     - SMTP_FROM=info@lxhome.fr
-     - SMTP_PORT=22
-     - SMTP_SECURITY=off
-     - SMTP_TIMEOUT=15
-     - SMTP_USERNAME=info@lxhome.fr
-     - SMTP_PASSWORD=Vaultwarden2024@%
-     - DOMAIN=https://vaultwarden.lxhome.fr
+#!/bin/bash
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  -v /:/host \
+  portainer/agent:2.27.4
